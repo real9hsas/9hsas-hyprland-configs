21f1870b0e4322c9916351d7fe92b18d3db4510e
9hsas <KrypZero@Proton.ME>
2026-08-06 21:33:39 +0200

    Create packages.sh

---
+++ packages.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+# If you want to add `brightnessctl` for laptops, you may can edit it
+# If you want its packages if you want it, you may can edit it
+# Or leave to default
+
+sudo pacman -Sy --noconfirm hyprland hyprshot hyprlauncher hyprpaper waybar hyprpolkitagent xdg-desktop-portal-hyprland pipewire mako pavucontrol kitty
