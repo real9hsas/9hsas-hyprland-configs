3218f24dfd710bcdcb857d074dc1df301659888b
9hsas <KrypZero@Proton.ME>
2026-08-18 23:28:38 +0200

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -4,4 +4,4 @@
 # If you want its packages if you want it, you may can edit it
 # Or leave to default
 
-sudo pacman -Sy --noconfirm hyprland hyprshot hyprlauncher hyprpaper waybar hyprpolkitagent xdg-desktop-portal-hyprland pipewire mako pavucontrol kitty
+sudo pacman -Sy --noconfirm hyprland hyprshot hyprlauncher hyprpaper waybar hyprpolkitagent xdg-desktop-portal-hyprland pipewire dusnt pavucontrol kitty
